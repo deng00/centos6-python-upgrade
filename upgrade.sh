f35548378000eb520a602ce37e5f699a353a0017
Danny0 <peablog@qq.com>
2017-04-02 13:57:57 +0800

    remove setuptool

--- upgrade.sh
+++ upgrade.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
-python2.7 -V
+
+# update yum
+yum -y update
+
 # pkg depend
-yum install -y gcc zlib-devel bzip2-devel openssl-devel xz-libs wget
+yum install -y gcc zlib-devel bzip2-devel openssl-devel xz-libs wget 
+
 # download python
 wget --no-check-certificate http://www.python.org/ftp/python/2.7.12/Python-2.7.12.tar.xz 
 xz -d Python-2.7.12.tar.xz
 tar -xvf Python-2.7.12.tar
 
-# 进入目录:
+# install py
 cd Python-2.7.12
-# 运行配置 configure:
+# configure:
 ./configure --prefix=/usr/local
-# 编译安装:
 make
 sudo make install
-# 修改链接
+
+# change bin
 ln -sf /usr/local/bin/python2.7  /usr/bin/python
-# 查看新版本
+# check
 python -V
 which python
-cd ../
-# 获取setuptools软件包
-wget --no-check-certificate https://pypi.python.org/packages/source/s/setuptools/setuptools-1.4.2.tar.gz
-# 解压:
-tar -xvf setuptools-1.4.2.tar.gz
-cd setuptools-1.4.2
-# 使用 Python 2.7 安装 setuptools
-python2.7 setup.py install
 
-# 安装pip
+# install pip
 curl https://bootstrap.pypa.io/get-pip.py | python2.7 -
 
-#修改yum
+# fix yum
 sed -i "s/\/usr\/bin\/python/\/usr\/bin\/python2.6/" /usr/bin/yum
+
+# end
